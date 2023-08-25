3a66cc08c43728c2c06f6504bc0790eb95a35872
tvntran1310 <truongvntran@gmail.com>
2023-08-25 09:00:14 +0700

    create login screen

---
+++ lib/inventory-management/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        title: Text('QUẢN LÝ KHO LKN'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-// import 'package:namer_app/ui-app/22-08-23/pages/menu_page.dart';
 import 'package:provider/provider.dart';
-// import 'ui-app/21-08-23/login_page.dart';
-import 'ui-app/22-08-23/pages/intro_page.dart';
+import 'inventory-management/homepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,10 +20,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'App',
         home: MyHomePage(),
-        routes: {
-          '/intropage': (context) => const IntroPage(),
-          // '/menupage': (context) => const MenuPage(),
-        },
       ),
     );
   }
@@ -41,25 +35,149 @@ class MyHomePage extends StatelessWidget {
     // var appState = context.watch<MyAppState>();
 
     return Scaffold(
-      body: SafeArea(
-          child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the DetailsPage
-                Navigator.push(
-                  context,
-                  // MaterialPageRoute(builder: (context) => LoginPage()),
-                  MaterialPageRoute(builder: (context) => IntroPage()),
-                );
-              },
-              child: Text('Go to Details'),
-            ),
-          ],
+      backgroundColor: Colors.grey,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /* align center - column */
+              // logo Thaco / image
+              Center(
+                child: Image(
+                  image: AssetImage(
+                    'lib/images/thaco/LOGO_INDUSTRIES.png',
+                  ),
+                  height: MediaQuery.of(context).size.width - 150,
+                  width: MediaQuery.of(context).size.width - 150,
+                  fit: BoxFit.contain,
+                ),
+              ),
+
+              // title
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: Text(
+                      'MNV/Email:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+
+              // textfield email
+              Container(
+                alignment: Alignment.center,
+                height: 50,
+                margin: EdgeInsets.only(
+                  top: 10,
+                  bottom: 30,
+                ),
+                // color: Colors.white,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Nhập MNV/Email',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                    ),
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(
+                      Icons.person,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+
+              // textfield password
+              // title
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 15),
+                    child: Text(
+                      'Password:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // textfield email
+              Container(
+                alignment: Alignment.center,
+                height: 50,
+                margin: EdgeInsets.only(
+                  top: 10,
+                  bottom: 30,
+                ),
+                // color: Colors.white,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Nhập Password',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                    ),
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(
+                      Icons.lock,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+
+              // button login
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    foregroundColor: Color.fromARGB(255, 0, 0, 0),
+                    minimumSize: Size(350, 50),
+                  ),
+                  child: Text(
+                    'Đăng nhập',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
